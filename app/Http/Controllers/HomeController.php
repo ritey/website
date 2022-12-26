8753e879784ce26122f64219c0bfe3bc8a7fcfc2
David Wright <dave@ritey.com>
2022-12-26 22:28:07 +0000

    Remove badge

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,56 +21,6 @@ class HomeController extends Controller
         return view('pages.home', compact('vars'))->render();
     }
 
-    public function badge()
-    {
-        $state = hash('sha256', microtime(true).rand()); // Generate a random state
-        $_SESSION['oauth2_state'] = $state; // Save the state
-        $params = [
-            'response_type' => 'code',
-            'client_id' => 'y2sI8a0W3CZb5mQ8gKFIOz3VvXiVZR5C',
-            'redirect_uri' => 'https://ritey.com/badge/auth/callback',
-            'scope' => 'openid',
-            'state' => $state,
-        ];
-        $authUrl = 'https://idea:gold@dev.idea.org.uk/oauth/authorize?'.
-        http_build_query($params);
-        header("Location: {$authUrl}"); // Redirect to iDEA to authorize
-
-        exit;
-    }
-
-    public function badgeCallback()
-    {
-        $state = $_GET['state'];
-        $code = $_GET['code'];
-        if (!isset($code)) {
-            exit('Failed to get an authorization code');
-        }
-        if (isset($state) && $state !== $_SESSION['oauth2_state']) { // Check the state is valid
-            session_destroy();
-
-            exit('OAuth2 invalid state!');
-        }
-        $client = new \GuzzleHttp\Client();
-        $res = $client->request(
-            'POST',
-            'https://dev.idea.org.uk/oauth/token',
-            [
-                'form_params' => [
-                    'client_id' => 'y2sI8a0W3CZb5mQ8gKFIOz3VvXiVZR5C',
-                    'client_secret' => 'Lwn8fVuT2OCao49GbLDan4D05fc3fmQeCd7KEx4y',
-                    'redirect_uri' => 'https://ritey.com/badge/auth/callback',
-                    'code' => $code,
-                    'grant_type' => 'authorization_code',
-                ],
-            ]
-        );
-        $json = json_decode($res->getBody());
-        $_SESSION['oauth2_access_token'] = $json->access_token;
-        $_SESSION['oauth2_id_token'] = $json->id_token;
-        dd($_SESSION);
-    }
-
     public function cv()
     {
         $vars = [
